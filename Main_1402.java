55cce9c5646b22e55972eda014491ae499892670
shinsung3 <tlstjd436@naver.com>
2019-08-07 00:18:44 +0900

    1402 ﻿아무래도이문제는A번난이도인것같다

---
+++ Main_1402.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(src);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			System.out.println("yes");
+		}
+	}
+
+	private static String src = "1\r\n" + "6 5";
+}
