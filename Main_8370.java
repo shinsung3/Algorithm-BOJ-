6c663536a73dc86e3da53536bfad1d58f993e97a
shinsung <tlstjd436@naver.com>
2020-09-30 17:11:42 +0900

    [Bronze 5] 8370 Plane

---
+++ Main_8370.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main_8370 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+//		sc = new Scanner(src);
+		int k1 = sc.nextInt();
+		int n1 = sc.nextInt();
+		int k2 = sc.nextInt();
+		int n2 = sc.nextInt();
+
+		System.out.println(k1 * n1 + k2 * n2);
+	}
+
+	private static String src = "2 5 3 20";
+}
