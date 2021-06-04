aba859adb6308fa792f14d0ef82ed473cb53dcff
nustar <tlstjd436@naver.com>
2021-06-04 19:13:00 +0900

    8061 bitmap [Silver3]

--- com.algorithm.java/src/com/algorithm/java/Main_8061_silver3.java
+++ com.algorithm.java/src/com/algorithm/java/Main_8061_silver3.java
@@ -1,10 +1,10 @@
-package com.algo.before;
+package com.algorithm.java;
 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main_8061 {
+public class Main_8061_silver3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
