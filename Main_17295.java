bc93cf41e46009c62b53352f620edd8c97b58dd2
nustar <tlstjd436@naver.com>
2021-06-04 20:10:01 +0900

    17295 엔드게임 스포일러

---
+++ Main_17295.java
@@ -0,0 +1,11 @@
+package com.algo.before;
+
+import java.util.Scanner;
+
+public class Main_17295 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		System.out.println("Avengers: Endgame");
+	}
+}
