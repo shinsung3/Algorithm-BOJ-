a24223cd3a64776108a3838d2c631665dc2d0391
nustar <tlstjd436@naver.com>
2021-06-13 00:40:33 +0900

    7569 토마토

--- com.algorithm.java/src/com/algo/before/Main_7569_silver1.java
+++ com.algorithm.java/src/com/algo/before/Main_7569_silver1.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main_7569 {
+public class Main_7569_silver1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 //		sc = new Scanner(src);
