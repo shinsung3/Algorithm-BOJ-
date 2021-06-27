0f0b8b60474a40095a641034d9b1018e3111569b
nustar <tlstjd436@naver.com>
2021-06-27 11:07:05 +0900

    16170 오늘의 날짜는?

---
+++ Main_16170.java
@@ -0,0 +1,9 @@
+package com.algorithm.java;
+
+public class Main_16170 {
+	public static void main(String[] args) {
+		System.out.println(2021);
+		System.out.println(06);
+		System.out.println(27);
+	}
+}
