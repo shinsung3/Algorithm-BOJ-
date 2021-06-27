452acc2a4752edfecd8a4ef544b84b41cddea9f6
nustar <tlstjd436@naver.com>
2021-06-27 10:48:06 +0900

    16394 홍익대학교

---
+++ Main_16394.java
@@ -0,0 +1,11 @@
+package com.algo.before;
+
+import java.util.Scanner;
+
+public class Main_16394 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a-1946);
+	}
+}
