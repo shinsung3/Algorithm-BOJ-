5475588774a5eb4d7fcf62b505aad8be47a839ba
nustar <tlstjd436@naver.com>
2021-07-02 19:41:59 +0900

    16430 제리와 톰

--- Main_16430.java
+++ Main_16430.java
@@ -2,15 +2,12 @@ package com.algorithm.java;
 
 import java.util.Scanner;
 
-public class Main_21300 {
+public class Main_16430 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
-		int f = sc.nextInt();
-		System.out.println(a*5 + b*5 + c*5 + d*5 + e*5 + f*5);
+		
+		System.out.println((b-a) +" "+b);
 	}
 }
---
+++ com.algorithm.java/src/com/algorithm/java/Main_16430.java
@@ -0,0 +1,13 @@
+package com.algorithm.java;
+
+import java.util.Scanner;
+
+public class Main_16430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println((b-a) +" "+b);
+	}
+}
