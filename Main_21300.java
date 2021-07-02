927e013863a4ae64d25c332030d5bf0d2208b756
nustar <tlstjd436@naver.com>
2021-07-02 19:30:31 +0900

    21300 Bottle Return

---
+++ Main_21300.java
@@ -0,0 +1,16 @@
+package com.algorithm.java;
+
+import java.util.Scanner;
+
+public class Main_21300 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		System.out.println(a*5 + b*5 + c*5 + d*5 + e*5 + f*5);
+	}
+}
