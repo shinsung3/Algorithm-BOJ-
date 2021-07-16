cd09043703fe9c31afcdbbcd966264cfa169fe77
nustar <tlstjd436@naver.com>
2021-07-16 19:29:55 +0900

    14645 와이버스 부릉부릉

---
+++ Main_14645.java
@@ -0,0 +1,18 @@
+package com.algorithm.java;
+
+import java.util.Scanner;
+
+public class Main_14645 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				sc.nextInt();
+			}
+		}
+		System.out.println("ºñ¿ÍÀÌ");
+	}
+}
