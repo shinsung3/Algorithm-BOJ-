97d633b8d9e2920e7af2af1919216d5d8653dc8a
nustar <tlstjd436@naver.com>
2021-07-16 19:46:15 +0900

    15439 Vera and Outfits

---
+++ Main_15439.java
@@ -0,0 +1,11 @@
+package com.algorithm.java;
+
+import java.util.Scanner;
+
+public class Main_15439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N*(N-1));
+	}
+}
