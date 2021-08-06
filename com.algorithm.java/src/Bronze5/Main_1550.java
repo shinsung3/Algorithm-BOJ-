a891e337b4aa589e895602cd30a844e11f18c016
nustar <tlstjd436@naver.com>
2021-08-06 20:04:48 +0900

    1550 16진수

---
+++ com.algorithm.java/src/Bronze5/Main_1550.java
@@ -0,0 +1,15 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Main_1550 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+//		sc = new Scanner(src);
+		String line = sc.nextLine();
+		int ans = Integer.parseInt(line, 16);
+		System.out.println(ans);
+	}
+
+	private static String src = "A";
+}
