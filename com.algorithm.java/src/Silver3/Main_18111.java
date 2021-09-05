73ca02b4f700cd9abdedb89b9b869dc21fbd0f30
nustar <tlstjd436@naver.com>
2021-09-05 16:14:55 +0900

    18111 마인크래프트

--- com.algorithm.java/src/Silver3/Main_18111.java
+++ com.algorithm.java/src/Silver3/Main_18111.java
@@ -1,3 +1,5 @@
+package Silver3;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -13,46 +15,46 @@ public class Main_18111 {
 		int answer = Integer.MAX_VALUE;
 		int height = 0;
 		int map[][] = new int[N][M];
-		int max= 0;
-		int min= Integer.MAX_VALUE;
-		for(int i = 0 ; i < N ; i ++) {
+		int max = 0;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < M ; j++) {
+			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				max = Math.max(max,map[i][j]);
+				max = Math.max(max, map[i][j]);
 				min = Math.min(min, map[i][j]);
 			}
 		}
-		
-		for(int i = max ; i >= min ; i--) {
+
+		for (int i = max; i >= min; i--) {
 			int time = 0;
 			int bag = inMyBag;
-			
-			for(int y = 0 ; y < N ; y ++) {
-				for(int  x= 0 ; x < M ; x++) {
-					if(map[y][x] == i) 
-                        continue;
+
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < M; x++) {
+					if (map[y][x] == i)
+						continue;
 					else {
 						int diff = Math.abs(i - map[y][x]);
-						if(i > map[y][x]) {
+						if (i > map[y][x]) {
 							bag -= diff;
-							time +=diff;
+							time += diff;
 						} else {
 							bag += diff;
-							time += (2*diff);
+							time += (2 * diff);
 						}
-						
+
 					}
 				}
 			}
-			if(bag< 0) {
+			if (bag < 0) {
 				continue;
 			}
-			if(time < answer) {
+			if (time < answer) {
 				answer = time;
 				height = i;
 			}
 		}
-		System.out.println(answer + " " + height );
+		System.out.println(answer + " " + height);
 	}
 }
